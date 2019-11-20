2dff0bf8dc26ede408ce6c5f1aca2571b3ef528c
Lucas Nascimento <csalucasnascimento@gmail.com>
2019-11-20 13:18:14 +1100

    sms message

--- src/functions/sns/subscribe-to-topic.js
+++ src/functions/sns/subscribe-to-topic.js
@@ -1,9 +1,9 @@
-const messageService = require('../../services/message.service')
+const smsService = require('../../services/sms.service')
 const { success, failure } = require('../../helpers/response.utils')
 
 module.exports.main = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false
-  messageService
+  smsService
     .subscribeToTopic(JSON.parse(event.body))
     .then((data) => callback(null, success(data)))
     .catch((err) => callback(null, failure(err)))
